ebbe484924feed2bb43adbb44a701a11c8cb5de5
selimolmaz <selimolmaz3.1415@gmail.com>
2024-10-29 02:11:11 +0300

    side bar düzeltildi

--- src/component/home_page/Sidebar/SidebarElement.js
+++ src/component/home_page/Sidebar/SidebarElement.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function SideBarElement({ name, onClick, index }) {
     // Index'in çift veya tek olmasına göre renk belirle
-    const color = index % 2 === 0 ? '#000000': '#808080';
+    const color = index % 2 === 0 ? 'rgb(255, 201, 101)': 'rgb(255, 168, 6)';
 
     return (
         <button className="bar-element" style={{ backgroundColor: color }} onClick={onClick}>
